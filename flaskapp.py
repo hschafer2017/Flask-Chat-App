2f69bf28db51d3617a13191b8fca31981e78ff6d
Haley Schafer <hschafer@umich.edu>
2018-05-25 11:28:51 +0000

    Files and Settings added for Heroku deployment

--- flaskapp.py
+++ flaskapp.py
@@ -45,4 +45,6 @@ def add_message(username):
     f.close()
             
     return redirect(username)
-app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+    
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
