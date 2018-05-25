d99fe02340dbfc98edf7617086ae7e79f7f91b07
Haley Schafer <hschafer@umich.edu>
2018-05-25 09:40:53 +0000

    filter words and auto refresh functions added

--- flaskapp.py
+++ flaskapp.py
@@ -5,7 +5,7 @@ import datetime
 app = Flask(__name__)
 
 messages=[]
-
+    
 @app.route('/')
 def get_index():
     return render_template('index.html')
@@ -24,15 +24,25 @@ def add_message(username):
     timing = datetime.datetime.now()
     text = request.form["message"]
     times = timing.strftime("%H:%M")
+    
+    
+    naughty_words = ['fuck', 'shit', 'ass']
+    words = text.split()
+    words = [word[0] + "*" * len(word[1:]) if word.lower() in naughty_words else word for word in words]
+    
+    text = " ".join(map(str,words))
+        
+    
     message = {
         'time' : times,
         'sender' : username, 
         'body' : text
     }
+    
     messages.append(message)
     f = open("messages.txt", "a")  
     lines = f.write(str(messages)+"\n")
-    f.close()                                      
-    return redirect(username) 
-    
+    f.close()
+            
+    return redirect(username)
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
